cf380f4f295017a9ac3f6a735f918d7ccc2a0110
dandye <dye.daniel@gmail.com>
2013-11-10 10:53:40 -0500

    fix fubar on casts/movies

--- transax_experiments.sql
+++ transax_experiments.sql
@@ -17,3 +17,40 @@ select count(*) from genres;
 ROLLBACK gen_trx;
 select count(*) from genres;
 -- 128
+
+/*
+SET ISOLATION allows a user to change the isolation level. 
+Valid levels are: 
+ * SERIALIZABLE
+ * REPEATABLE REA
+ * READ COMMITTED
+ * READ UNCOMMITTED. */
+ 
+ROLLBACK;
+set transaction name 'gen_trx';
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+/* ------------ */
+
+select count(*) from genres;
+-- 128
+insert into genres
+VALUES (60,'Bar');
+select count(*) from genres;
+-- 129
+COMMIT;
+select count(*) from genres;
+-- 129
+
+
+
+
+ALTER SESSION SET ISOLATION_LEVEL READ COMMITTED;   
+ 
+ALTER SESSION 
+SET ISOLATION LEVEL
+READ COMMITTED;
+ 
+ 
+ 
+ 
+ 
